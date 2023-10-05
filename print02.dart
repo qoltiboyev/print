8150e18dc3101322a350cdc0459ab93c6cc3bd3a
qoltiboyev <qamariddinoltiboyevdev@gmail.com>
2023-10-05 15:30:01 +0500

    malumotlarni chiqar

---
+++ print02.dart
@@ -0,0 +1,9 @@
+void main() {
+  print('FullName Oltiboyev Qamariddin');
+  print('My age 22');
+  print('My State Ubekiston');
+  print('My Region Jizzax');
+  print('My District Baxmal');
+  print('My Neighborhood Bunyodkor');
+  print("My Village Tengali");
+}
